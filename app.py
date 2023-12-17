c25bb46a76a0031c73d815c7dc6e5f4a7d8d99fe
Mahesh Swaminathan <maheshswaminathan@Maheshs-MacBook-Air.local>
2023-12-17 03:55:00 -0500

    added tests

--- app.py
+++ app.py
@@ -1,105 +1,68 @@
-from flask import Flask,jsonify,request
-
+from flask import Flask, request, jsonify
 import secrets
-
-from secrets import randbelow
-from datetime import datetime
+import datetime
+from threading import Lock
 
 app = Flask(__name__)
+lock = Lock()
+posts = {}  # Global state to store posts
+post_id_counter = 1
 
-posts=[]
+# Endpoint #1: Create a post
+@app.route('/post', methods=['POST'])
+def create_post():
+    with lock:
+        try:
+            data = request.get_json()
+            if not isinstance(data, dict) or 'msg' not in data or not isinstance(data['msg'], str):
+                return jsonify({'err': 'Bad request'}), 400
 
-@app.get("/random/<int:sides>")
-def roll(sides):
-    if sides <= 0:
-        return { 'err': 'need a positive number of sides' }, 400
-    
-    return { 'num': randbelow(sides) + 1 }
+            post_id = generate_unique_id()
+            key = generate_random_key()
+            timestamp = get_current_timestamp()
 
-def generate_key():
-    # Generating a secure random key using secrets module
-    return secrets.token_urlsafe(32)
+            post = {'id': post_id, 'key': key, 'timestamp': timestamp, 'msg': data['msg']}
+            posts[post_id] = post
 
-@app.post("/post")
-def post():
-    try:
-        data = request.get_json()
+            return jsonify(post)
+        except Exception as e:
+            return jsonify({'err': str(e)}), 500
 
-        # Check if the request body is a JSON object and contains the 'msg' field
-        if not isinstance(data, dict) or 'msg' not in data or not isinstance(data['msg'], str):
-            return jsonify({"error": "Bad Request. 'msg' field missing or not a string."}), 400
+# Endpoint #2: Read a post
+@app.route('/post/<int:post_id>', methods=['GET'])
+def read_post(post_id):
+    with lock:
+        if post_id not in posts:
+            return jsonify({'err': 'Not found'}), 404
 
-        # Generate unique id
-        post_id = len(posts) + 1
+        post = posts[post_id]
+        return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg']})
 
-        # Generate a unique random key
-        key = generate_key()
-
-        # Get the current timestamp in ISO 8601 format
-        timestamp = datetime.utcnow().isoformat()
-
-        # Create the post
-        post = {
-            "id": post_id,
-            "key": key,
-            "timestamp": timestamp,
-            "msg": data['msg']
-        }
-
-        # Add the post to the list
-        posts.append(post)
-
-        # Return the response
-        response = {
-            "id": post_id,
-            "key": key,
-            "timestamp": timestamp
-        }
-
-        return jsonify(response), 201
-
-    except Exception as e:
-        return jsonify({"error": "Internal Server Error","error":e}), 500
-    
-
-@app.get('/post/<int:post_id>')
-def get_post(post_id):
-    post = next((p for p in posts if p['id'] == post_id), None)
-    if post:
-        # Return the response without the key
-        response = {
-            "id": post['id'],
-            "timestamp": post['timestamp'],
-            "msg": post['msg']
-        }
-        return jsonify(response)
-    else:
-        return jsonify({"error": "Post not found"}), 404
-    
+# Endpoint #3: Delete a post
 @app.route('/post/<int:post_id>/delete/<string:key>', methods=['DELETE'])
 def delete_post(post_id, key):
-    global posts
+    with lock:
+        if post_id not in posts:
+            return jsonify({'err': 'Not found'}), 404
 
-    # Find the post by ID
-    post = next((p for p in posts if p['id'] == post_id), None)
+        post = posts[post_id]
+        if post['key'] != key:
+            return jsonify({'err': 'Forbidden'}), 403
 
-    if post:
-        # Check if the provided key matches the key associated with the post
-        if key == post['key']:
-            # Delete the post
-            posts = [p for p in posts if p['id'] != post_id]
-            
-            # Return the same information as in the POST response
-            response = {
-                "id": post['id'],
-                "key": generate_key(),  # Generate a new key after deletion
-                "timestamp": post['timestamp']
-            }
+        del posts[post_id]
+        return jsonify({'id': post['id'], 'key': post['key'], 'timestamp': post['timestamp']})
 
-            return jsonify(response), 200
-        else:
-            # Key mismatch, return forbidden error
-            return jsonify({"error": "Forbidden. Invalid key"}), 403
-    else:
-        # Post not found, return not found error
-        return jsonify({"error": "Post not found"}), 404
+def generate_unique_id():
+    global post_id_counter
+    post_id = post_id_counter
+    post_id_counter += 1
+    return post_id
+
+def generate_random_key():
+    return secrets.token_urlsafe(32)
+
+def get_current_timestamp():
+    return datetime.datetime.utcnow().isoformat()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# uncomment the line below to install some-package
+# pip3 install some-package

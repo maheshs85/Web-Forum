e8361573203658e2a17a22876f8c60be0290e8ec
Mahesh Swaminathan <maheshswaminathan@Maheshs-MacBook-Air.local>
2023-12-17 15:56:55 -0500

    added extension 1

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from threading import Lock
 
 app = Flask(__name__)
 lock = Lock()
-posts = {}  # Global state to store posts
+users = {}
+posts = {}
 post_id_counter = 1
-
+    
 # Endpoint #1: Create a post
 @app.route('/post', methods=['POST'])
 def create_post():
@@ -17,11 +18,19 @@ def create_post():
             if not isinstance(data, dict) or 'msg' not in data or not isinstance(data['msg'], str):
                 return jsonify({'err': 'Bad request'}), 400
 
+            user_id = None
+            if 'user_id' in data and 'user_key' in data:
+                user_id = data['user_id']
+                user_key = data['user_key']
+
+                if user_id not in users or users[user_id]['key'] != user_key:
+                    return jsonify({'err': 'Unauthorized'}), 401
+            
             post_id = generate_unique_id()
             key = generate_random_key()
             timestamp = get_current_timestamp()
 
-            post = {'id': post_id, 'key': key, 'timestamp': timestamp, 'msg': data['msg']}
+            post = {'id': post_id, 'key': key, 'user_id': user_id, 'timestamp': timestamp, 'msg': data['msg']}
             posts[post_id] = post
 
             return jsonify(post)
@@ -36,7 +45,11 @@ def read_post(post_id):
             return jsonify({'err': 'Not found'}), 404
 
         post = posts[post_id]
-        return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg']})
+
+        user_id = None
+        if 'user_id' in post:
+            user_id = post['user_id']
+        return jsonify({'id': post['id'], 'user_id': user_id, 'timestamp': post['timestamp'], 'msg': post['msg']})
 
 # Endpoint #3: Delete a post
 @app.route('/post/<int:post_id>/delete/<string:key>', methods=['DELETE'])
@@ -46,12 +59,29 @@ def delete_post(post_id, key):
             return jsonify({'err': 'Not found'}), 404
 
         post = posts[post_id]
-        if post['key'] != key:
+        posts_key = post['key']
+
+        user_id = post['user_id']
+        if user_id != None:
+            user = users[user_id]
+            user_key = user['key']
+        else:
+            user_key = None
+            
+        if posts_key != key and user_key != key:
             return jsonify({'err': 'Forbidden'}), 403
 
         del posts[post_id]
-        return jsonify({'id': post['id'], 'key': post['key'], 'timestamp': post['timestamp']})
+        return jsonify({'id': post_id, 'user_id': user_id, 'timestamp': post['timestamp']})
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    with lock:
+        user_id = generate_unique_id()
+        user_key = generate_random_key()
+        users[user_id] = {'id': user_id, 'key': user_key}
+        return jsonify(users[user_id])
+    
 def generate_unique_id():
     global post_id_counter
     post_id = post_id_counter
@@ -65,4 +95,4 @@ def get_current_timestamp():
     return datetime.datetime.utcnow().isoformat()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
